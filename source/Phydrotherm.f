0122d690b608712eed13603d4c17dc1682e8ccc0
piermafrost <pierre.maffre@normalesup.org>
2024-11-14 17:04:50 +0100

    use parameter from "constant.f90"

--- source/Phydrotherm.f
+++ source/Phydrotherm.f
@@ -1,12 +1,12 @@
     subroutine Phydrotherm(t)
 !   *************************
 !   scavenging of P on iron oxides (hydrothermal)
+    use constante, only: akPhyd
     implicit none
     include 'combine_foam.inc'
 
     do i=1,nbasin
-        fhydP(i)=0.325d+10*var_diss(3,i)/1.5d-3*clo*phosss* &
-                 indice_deep(i)
+        fhydP(i)=akPhyd*var_diss(3,i)*clo*phosss*indice_deep(i)
     enddo
     return
     end
