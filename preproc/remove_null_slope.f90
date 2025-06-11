39cc1c544835510c1a116862b085ef399a68b2a6
piermafrost <pierre.maffre@normalesup.org>
2025-06-11 14:55:28 +0200

    modernize file (input file can be passed by user)

--- preproc/remove_null_slope.f90
+++ preproc/remove_null_slope.f90
@@ -1,20 +1,59 @@
+! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+! Use: `./remove_null_slope slope_file_name`
+! %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 program remove_null_slope
 use netcdf
 
-double precision, dimension(360,180):: slope
-integer:: ID, VID, ierr
+integer:: nx, ny, narg, fid, vid, dimids(2), ierr
+character(len=500):: filename
+double precision, dimension(:,:), allocatable:: slope
+double precision:: minslp
 
-ierr = nf90_open( 'slope1x1_nonzero.nc', NF90_WRITE , ID )
-print *, nf90_strerror(ierr)
-ierr = nf90_inq_varid( ID, 'slope', VID )
-print *, nf90_strerror(ierr)
-ierr = nf90_get_var( ID, VID, slope )
-print *, nf90_strerror(ierr)
-print *,  minval(minval( slope , 1 , slope>0 ),1)
-where (slope==0) slope = minval(minval( slope , 1 , slope>0 ),1)
-ierr = nf90_put_var( ID, VID, slope )
-print *, nf90_strerror(ierr)
-ierr = nf90_close(ID)
-print *, nf90_strerror(ierr)
+narg = command_argument_count()
+if (narg >= 1) then
+    call get_command_argument(1, filename)
+    if (narg > 1) write(unit=*, fmt='(A,I0,A)') 'warning: ignore ', narg-1, ' extra argument(s) passed to the script'
+else
+    write(unit=*, fmt='(A)', advance='no') 'Name of "slope" netCDF file: '
+    read(unit=*, fmt='(A)') filename
+end if
+
+! ============================================================================ !
+
+ierr = nf90_open(filename, NF90_WRITE , fid)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+
+ierr = nf90_inq_varid(fid, 'slope', vid)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+
+ierr = nf90_inquire_variable(fid, vid, dimids=dimids)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+ierr = nf90_inquire_dimension(fid, dimids(1), len=nx)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+ierr = nf90_inquire_dimension(fid, dimids(2), len=ny)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+
+!++++++++++++++++++++!
+allocate(slope(nx,ny))
+!++++++++++++++++++++!
+
+ierr = nf90_get_var(fid, vid, slope)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+
+!++++++++++++++++++++++++++++++++++++++++++++++++!
+minslp =  minval(minval(slope, 1, slope>0), 1)
+print *
+print *, 'minimum non-zero slope value: ', minslp
+print *
+where (slope==0) slope = minslp
+!++++++++++++++++++++++++++++++++++++++++++++++++!
+
+ierr = nf90_put_var(fid, vid, slope)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+ierr = nf90_close(fid)
+if (ierr/=NF90_NOERR) print *, nf90_strerror(ierr)
+
+deallocate(slope)
 
 end program
